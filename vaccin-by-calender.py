e7bec5140fda1c55e9c8baef351db7a3d5ba459c
Harshal Kulkarni <harshalk.91@gmail.com>
2021-05-02 15:07:37 +0530

    Filter by pincode

---
+++ vaccin-by-calender.py
@@ -0,0 +1,24 @@
+import requests
+from prettytable import PrettyTable
+
+
+pincode = "411030"
+api_url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/"
+
+
+t = PrettyTable(['Name', 'Pincode', 'Free/Paid', 'min_age_limit', 'vaccine', 'date', 'fees'])
+
+resp = requests.get(url=api_url + "calendarByPin" + "?pincode=" + pincode + "&date=03-05-2021").json()
+for i in resp.get('centers'):
+    for j in i.get('sessions'):
+        if i['fee_type'] == "Paid":
+            if 'vaccine_fees' in i:
+                for k in i['vaccine_fees']:
+                    t.add_row([i['name'], str(i['pincode']), i['fee_type'], str(j['min_age_limit']), j['vaccine'],
+                               j['date'], str(k.get('fee'))])
+
+        else:
+            t.add_row([i['name'], str(i['pincode']), i['fee_type'], str(j['min_age_limit']), j['vaccine'],
+                       j['date'], "Free"])
+
+print(t)
