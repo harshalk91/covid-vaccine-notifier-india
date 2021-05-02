09e941c338360f1ff66fa8d8e95c18fd13816dcb
Harshal Kulkarni <harshalk.91@gmail.com>
2021-05-02 17:13:45 +0530

    Corrected naming convention Minimum Age limit 18

--- vaccin-by-calender.py
+++ vaccin-by-calender.py
@@ -1,25 +1,30 @@
 import requests
 from prettytable import PrettyTable
+import datetime
 
-
-pincode = "110044"
-date = "03-05-2021"
+min_age = 18
+pincode = "530001"
+date = date = (datetime.datetime.now().date()).strftime("%d-%m-%Y")
 api_url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/"
 
+t = PrettyTable(['Name', 'Pincode', 'Free/Paid', 'min_age_limit', 'vaccine', 'date', 'available_capacity', 'fees'])
 
-t = PrettyTable(['Name', 'Pincode', 'Free/Paid', 'min_age_limit', 'vaccine', 'date', 'fees'])
+response = requests.get(url=api_url + "calendarByPin" + "?pincode={}".format(pincode) + "&date={}".format(date)).json()
+for center in response.get('centers'):
+    for session in center.get('sessions'):
+        if session['min_age_limit'] == 18:
+            if center['fee_type'] == "Paid":
+                if 'vaccine_fees' in center:
+                    for fee_list in center['vaccine_fees']:
+                        t.add_row(
+                            [center['name'], str(center['pincode']), center['fee_type'], str(session['min_age_limit']),
+                             session['vaccine'],
+                             session['date'], session['available_capacity'], str(fee_list.get('fee'))])
 
-resp = requests.get(url=api_url + "calendarByPin" + "?pincode={}".format(pincode) + "&date={}".format(date)).json()
-for i in resp.get('centers'):
-    for j in i.get('sessions'):
-        if i['fee_type'] == "Paid":
-            if 'vaccine_fees' in i:
-                for k in i['vaccine_fees']:
-                    t.add_row([i['name'], str(i['pincode']), i['fee_type'], str(j['min_age_limit']), j['vaccine'],
-                               j['date'], str(k.get('fee'))])
-
-        else:
-            t.add_row([i['name'], str(i['pincode']), i['fee_type'], str(j['min_age_limit']), j['vaccine'],
-                       j['date'], "Free"])
+            else:
+                t.add_row([center['name'], str(center['pincode']), center['fee_type'], str(session['min_age_limit']),
+                           session['vaccine'],
+                           session['date'], session['available_capacity'], "Free"])
 
 print(t)
+t.clear()
