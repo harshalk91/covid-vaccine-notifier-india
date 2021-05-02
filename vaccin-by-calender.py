0fba00d9ebd50ae1b189ffd0162cae1052b88efc
Harshal Kulkarni <harshalk.91@gmail.com>
2021-05-02 15:13:08 +0530

    Parameterized date

--- vaccin-by-calender.py
+++ vaccin-by-calender.py
@@ -2,13 +2,14 @@ import requests
 from prettytable import PrettyTable
 
 
-pincode = "411030"
+pincode = "110044"
+date = "03-05-2021"
 api_url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/"
 
 
 t = PrettyTable(['Name', 'Pincode', 'Free/Paid', 'min_age_limit', 'vaccine', 'date', 'fees'])
 
-resp = requests.get(url=api_url + "calendarByPin" + "?pincode=" + pincode + "&date=03-05-2021").json()
+resp = requests.get(url=api_url + "calendarByPin" + "?pincode={}".format(pincode) + "&date={}".format(date)).json()
 for i in resp.get('centers'):
     for j in i.get('sessions'):
         if i['fee_type'] == "Paid":
